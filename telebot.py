b483f91c21d7ae3746066f299d523453d2b67387
jmaris <jman6495@gmail.com>
2015-06-29 18:25:36 +0200

    Fixed getupdates

--- telebot.py
+++ telebot.py
@@ -1,12 +1,19 @@
 import requests
 import json
+#remembers bot token and creates a URL that can be used to connect to the API
 def authorise(authtoken):
 	global tokenurl
 	tokenurl = 'https://api.telegram.org/bot'+authtoken+'/'
-def getupdates(updateid,timeout,debug):
+
+#gets updates from the API and returns them as python readable data
+def getupdates(offset,timeout,debug):
 	updateurl=tokenurl+'getUpdates'
-	opts = {'update_id': updateid, 'timeout': timeout}
+	opts = {'offset': offset, 'timeout': timeout}
 	return(json.loads(requests.get(updateurl, params=opts).text))
+#gets bot name and info, returns as python readable data
 def getme():
 	updateurl=tokenurl+'getMe'
 	return(json.loads(requests.get(updateurl).text))
+#sends messages,optional parameters still need to be added (see API Documentation)
+def sendmessage(chatid,text,):
+	
