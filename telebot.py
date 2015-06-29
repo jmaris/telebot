e8974006590354d48aeec66de0abe34731315046
jmaris <jman6495@gmail.com>
2015-06-29 20:41:09 +0200

    Added keyboard generation and removal. Milestone 1

--- telebot.py
+++ telebot.py
@@ -10,13 +10,37 @@ def getupdates(offset='',timeout=0,debug=0):
 	updateurl=tokenurl+'getUpdates'
 	opts = {'offset': offset, 'timeout': timeout}
 	return(json.loads(requests.get(updateurl, params=opts).text))
+
 #gets bot name and info, returns as python readable data
 def getme():
 	updateurl=tokenurl+'getMe'
 	return(json.loads(requests.get(updateurl).text))
+
 #generates keyboards
 def keyboardmake(keyboardlist,resize=1,once=1,selective=''):
-	return("{\"keyboard\":"+json.dumps(keyboardlist).replace('"', '\"')+"}")
+	if(resize==1):
+		options="\"resize_keyboard\":true"
+	else:
+		options="\"resize_keyboard\":false"
+	if(once==1):
+		options=options+","+"\"one_time_keyboard\":true"
+	else:
+		options=options+","+"\"one_time_keyboard\":false"
+	if(selective==1):
+		options=options+","+"\"selective\":1"
+	if(selective==2):
+		options=options+","+"\"selective\":2"
+	print(options)
+	return("{\"keyboard\":"+json.dumps(keyboardlist).replace('"', '\"')+options+"}")
+#sends a message to destroy existing keyboards
+def keyboarddestroy(selective=''):
+	if(selective==1):
+		options=",\"selective\":1"
+	if(selective==2):
+		options=",\"selective\":2"
+	if(selective==''):
+		options=''
+	return("{\"hide_keyboard\":true"+options+"}")
 #sends messages
 def sendmessage(chatid,text,replyto='',replymarkup=''):
 	updateurl=tokenurl+'sendMessage'
